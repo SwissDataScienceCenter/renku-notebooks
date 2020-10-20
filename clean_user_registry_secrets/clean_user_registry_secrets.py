622ca8fd668993cb7ec51dc4227ccb2fab03d5ca
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2020-10-21 00:27:06 +0200

    remove try/except for k8s api

--- clean_user_registry_secrets/clean_user_registry_secrets.py
+++ clean_user_registry_secrets/clean_user_registry_secrets.py
@@ -34,25 +34,18 @@ from kubernetes.config.incluster_config import (
 
 def find_pod_by_secret(secret, k8s_client):
     """Find the user jupyterhub podname based on the registry pull secret."""
-    namespace = secret.metadata.namespace
     label_keys = ['renku.io/commit-sha', 'renku.io/projectName', 'renku.io/username']
     label_selector = []
     for label_key in label_keys:
         label_selector.append(f"{label_key}={secret.metadata.labels[label_key]}")
     label_selector = ",".join(label_selector)
 
-    try:
-        pod_list = k8s_client.list_namespaced_pod(
-            secret.metadata.namespace,
-            label_selector=label_selector,
-        )
-    except client.rest.ApiException:
-        logging.warning(
-            f"Namespace {namespace} does not exist or the cluster is unreachable."
-        )
-    else:
-        if len(pod_list.items) > 0:
-            return pod_list.items[0].metadata.name
+    pod_list = k8s_client.list_namespaced_pod(
+        secret.metadata.namespace,
+        label_selector=label_selector,
+    )
+    if len(pod_list.items) > 0:
+        return pod_list.items[0].metadata.name
     return None
 
 
@@ -64,63 +57,53 @@ def remove_user_registry_secret(namespace, k8s_client, min_secret_age_hrs=0.25):
         f"Checking for user registry secrets whose "
         f"names match the regex: {secret_name_regex}"
     )
-    try:
-        secret_list = k8s_client.list_namespaced_secret(namespace)
-    except client.rest.ApiException:
-        logging.warning(
-            f"Namespace {namespace} does not exist or the cluster is unreachable."
-        )
-    else:
-        for secret in secret_list.items:
-            # loop through secrets and find ones that match the predefined regex
-            secret_name = secret.metadata.name
-            secret_name_match = re.match(secret_name_regex, secret_name)
-            # calculate secret age
-            tz = secret.metadata.creation_timestamp.tzinfo
-            secret_age = datetime.now(tz=tz) - secret.metadata.creation_timestamp
-            min_secret_age = timedelta(hours=min_secret_age_hrs)
-            if (
-                secret_name_match is not None
-                and secret.type == "kubernetes.io/dockerconfigjson"
-                and secret.metadata.labels.get("component") == "singleuser-server"
-                and reduce(  # check that label keys for sha, project and username are present
-                    lambda acc, val: acc and (val in secret.metadata.labels.keys()),
-                    label_keys,
-                    True,
-                )
-            ):
-                podname = find_pod_by_secret(secret, k8s_client)
-                if podname is None:
-                    # pod does not exist, delete if secret is old enough
-                    if secret_age > min_secret_age:
-                        logging.info(
-                            f"User pod that used secret {secret_name} does not exist, "
-                            f"deleting secret as it is older "
-                            f"than the {min_secret_age_hrs} hours threshold"
-                        )
-                        k8s_client.delete_namespaced_secret(secret_name, namespace)
-                else:
-                    # check if the pod has the expected annotations and is running or succeeded
-                    # no need to check for secret age because we are certain secret has been used
-                    try:
-                        pod = k8s_client.read_namespaced_pod(podname, namespace)
-                    except client.rest.ApiException:
-                        logging.warning(
-                            f"Cannot find pod {podname}, "
-                            f"secret {secret_name} has not been removed."
-                        )
-                    else:
-                        if (
-                            pod.metadata.labels.get("app") == "jupyterhub"
-                            and pod.metadata.labels.get("component")
-                            == "singleuser-server"
-                            and pod.status.phase in ["Running", "Succeeded"]
-                        ):
-                            logging.info(
-                                f"Found user pod {podname} that used the secret, "
-                                f"deleting secret {secret_name}."
-                            )
-                            k8s_client.delete_namespaced_secret(secret_name, namespace)
+    secret_list = k8s_client.list_namespaced_secret(namespace)
+    logging.warning(
+        f"Namespace {namespace} does not exist or the cluster is unreachable."
+    )
+    for secret in secret_list.items:
+        # loop through secrets and find ones that match the predefined regex
+        secret_name = secret.metadata.name
+        secret_name_match = re.match(secret_name_regex, secret_name)
+        # calculate secret age
+        tz = secret.metadata.creation_timestamp.tzinfo
+        secret_age = datetime.now(tz=tz) - secret.metadata.creation_timestamp
+        min_secret_age = timedelta(hours=min_secret_age_hrs)
+        if (
+            secret_name_match is not None
+            and secret.type == "kubernetes.io/dockerconfigjson"
+            and secret.metadata.labels.get("component") == "singleuser-server"
+            and reduce(  # check that label keys for sha, project and username are present
+                lambda acc, val: acc and (val in secret.metadata.labels.keys()),
+                label_keys,
+                True,
+            )
+        ):
+            podname = find_pod_by_secret(secret, k8s_client)
+            if podname is None:
+                # pod does not exist, delete if secret is old enough
+                if secret_age > min_secret_age:
+                    logging.info(
+                        f"User pod that used secret {secret_name} does not exist, "
+                        f"deleting secret as it is older "
+                        f"than the {min_secret_age_hrs} hours threshold"
+                    )
+                    k8s_client.delete_namespaced_secret(secret_name, namespace)
+            else:
+                # check if the pod has the expected annotations and is running or succeeded
+                # no need to check for secret age because we are certain secret has been used
+                pod = k8s_client.read_namespaced_pod(podname, namespace)
+                if (
+                    pod.metadata.labels.get("app") == "jupyterhub"
+                    and pod.metadata.labels.get("component")
+                    == "singleuser-server"
+                    and pod.status.phase in ["Running", "Succeeded"]
+                ):
+                    logging.info(
+                        f"Found user pod {podname} that used the secret, "
+                        f"deleting secret {secret_name}."
+                    )
+                    k8s_client.delete_namespaced_secret(secret_name, namespace)
 
 
 def float_gt_zero(number):
