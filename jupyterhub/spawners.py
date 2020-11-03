95e3d5335995b85ab277ec967f199a80633f6826
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2020-11-03 17:36:29 +0100

    update annotations

--- jupyterhub/spawners.py
+++ jupyterhub/spawners.py
@@ -286,7 +286,8 @@ class RenkuKubeSpawner(SpawnerMixin, KubeSpawner):
             RENKU_ANNOTATION_PREFIX + "commit-sha": options.get("commit_sha"),
             RENKU_ANNOTATION_PREFIX + "repository": repository_url,
             RENKU_ANNOTATION_PREFIX + "image": options.get("image"),
-            RENKU_ANNOTATION_PREFIX + "default_image_used": options.get("default_image_used"),
+            RENKU_ANNOTATION_PREFIX
+            + "default_image_used": server_options.get("default_image_used"),
         }
 
         # add username to labels
