426df000ef9928a6112566ff226d9263ef8bfbfd
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2021-01-06 16:23:25 +0100

    fix: deletion timestamp attribute name

--- renku_notebooks/util/kubernetes_.py
+++ renku_notebooks/util/kubernetes_.py
@@ -156,7 +156,7 @@ def _get_all_user_servers(user):
                 ready = (
                     ready
                     and status.ready
-                    and getattr(pod.metadata, "deletionTimestamp", None) is None
+                    and getattr(pod.metadata, "deletion_timestamp", None) is None
                 )
         except (IndexError, TypeError):
             ready = False
