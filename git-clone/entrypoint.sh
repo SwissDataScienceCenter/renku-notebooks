85974f78bf6a007d14d8aeb5ef1a70e7d0f50867
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2021-01-07 22:35:00 +0100

    squashme: improve some wording in comments

--- git-clone/entrypoint.sh
+++ git-clone/entrypoint.sh
@@ -35,60 +35,62 @@ git fetch origin
 git checkout ${BRANCH} || git checkout -b ${BRANCH}
 git submodule init && git submodule update
 
-# If option is set, try to recover from a relevant autosave branch.
+# If this option is set, try to recover from a relevant autosave branch.
 if [ "${GITLAB_AUTOSAVE}" == "1" ] ; then
 
-  # Go through available branches and find the appropriate autosave branch
+  # Go through all available branches and find the appropriate autosave branch.
   REMOTES_ORIGIN="remotes/origin/"
   AUTOSAVE_BRANCH_PREFIX="renku/autosave/$JUPYTERHUB_USER"
 
-  # Note that the () turn the output into an array
+  # Note that the () turn the output into an array.
   ALL_BRANCHES=(`git branch -a `)
   for branch in "${ALL_BRANCHES[@]}"
   do
-    # TODO: What if there were are multiple matches here?
+    # It's not scrictly impossible that we will have more than one branch matching
+    # here, but RenkuLab should preven users from creating more than one autsave
+    # branch per user/branch/commmit tuple.
     if [[ $branch == *"${REMOTES_ORIGIN}${AUTOSAVE_BRANCH_PREFIX}/${BRANCH}/${COMMIT_SHA:0:7}"* ]] ; then
         AUTOSAVE_REMOTE_BRANCH=${branch// /}
         break
     fi
   done
 
-  # If no autosave branch was found, simply reset to the selected commit
+  # If no autosave branch was found, simply reset to the selected commit.
   if [ -z "$AUTOSAVE_REMOTE_BRANCH" ] ; then
     git reset --hard $COMMIT_SHA
   else
 
     IFS='/' read -r -a AUTOSAVE_REMOTE_BRANCH_ITEMS <<< "$AUTOSAVE_REMOTE_BRANCH"
 
-    # Check if found autosave branch has valid format, fail otherwise
+    # Check if the found autosave branch has a valid format, fail otherwise
     if [ "${#AUTOSAVE_REMOTE_BRANCH_ITEMS[@]}" -lt 7 ] ; then
       echo "Auto-save branch is in the wrong format; cannot recover the state from that branch" >&2
       return 1
     fi
 
-    # Reset file tree to auto-saved state
+    # Reset the file tree to the auto-saved state.
     git reset --hard $AUTOSAVE_REMOTE_BRANCH
 
-    # Reset HEAD to last committed change prior to autosave
+    # Reset HEAD to the last committed change prior to the autosave commit.
     PRE_SAVE_LOCAL_COMMIT_SHA=${AUTOSAVE_REMOTE_BRANCH_ITEMS[7]}
     git reset --soft $PRE_SAVE_LOCAL_COMMIT_SHA
 
-    # Unstage all modified files
+    # Unstage all modified files.
     git reset HEAD .
 
-    # Delete autosave branch both remote and local
+    # Delete the autosave branch both remotely and locally.
     AUTOSAVE_LOCAL_BRANCH=${AUTOSAVE_REMOTE_BRANCH/$REMOTES_ORIGIN/''}
     git push origin :"$AUTOSAVE_LOCAL_BRANCH"
   fi
 fi
 
-# Set up repo to communicate through git https proxy
-# Note: The proxy will still verify the certificates of
-#       the connection to the git server.
+# Configure the repo such that the git client will communicate with GitLab
+# through the https proxy.
+# Note: The proxy will still verify the certificates of the connection to the git server.
 git config http.proxy http://localhost:8080
 git config http.sslVerify false
 git config --unset credential.helper
 rm .git/credentials
 
-# Finally, set permissions correctly for main container
+# Finally, set the correct permissions for the main container.
 chown ${USER_ID}:${GROUP_ID} -Rc ${MOUNT_PATH}
