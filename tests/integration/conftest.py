22c854f07f428ea0840a01c0111af9a6baeb64ad
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2021-07-20 09:59:40 +0200

    squashme: add debug session

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -270,9 +270,6 @@ def launch_session(
                     minutes=timeout_mins
                 ):
                     print("Witing for server to be ready timed out.")
-                    print(subprocess.check_output(
-                        ["kubectl", "describe", "pod", pod.metadata.name]
-                    ))
                     return None  # waiting for pod to fully become ready timed out
                 if pod_ready:
                     return response
