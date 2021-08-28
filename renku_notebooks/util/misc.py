babf7f9a0a44b362bcc8c1da9cf9f8bbf9e5aa46
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2021-08-28 15:52:25 +0200

    fix: fix telepresence mode for telepresence 1.x

--- renku_notebooks/util/misc.py
+++ renku_notebooks/util/misc.py
@@ -3,9 +3,12 @@ import os
 
 
 def read_server_options_ui():
-    server_options_file = os.getenv(
-        "NOTEBOOKS_SERVER_OPTIONS_UI_PATH",
-        "/etc/renku-notebooks/server_options/server_options.json",
+    server_options_file = os.path.join(
+        os.getenv("TELEPRESENCE_ROOT", "/"),
+        os.getenv(
+            "NOTEBOOKS_SERVER_OPTIONS_UI_PATH",
+            "/etc/renku-notebooks/server_options/server_options.json",
+        ).lstrip("/"),
     )
     with open(server_options_file) as f:
         server_options = json.load(f)
@@ -14,9 +17,12 @@ def read_server_options_ui():
 
 
 def read_server_options_defaults():
-    server_options_file = os.getenv(
-        "NOTEBOOKS_SERVER_OPTIONS_DEFAULTS_PATH",
-        "/etc/renku-notebooks/server_options/server_defaults.json",
+    server_options_file = os.path.join(
+        os.getenv("TELEPRESENCE_ROOT", "/"),
+        os.getenv(
+            "NOTEBOOKS_SERVER_OPTIONS_DEFAULTS_PATH",
+            "/etc/renku-notebooks/server_options/server_defaults.json",
+        ).lstrip("/"),
     )
     with open(server_options_file) as f:
         server_options = json.load(f)
