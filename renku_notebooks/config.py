02950ddc2598bdac7662cc06bfdceceb2c33f78b
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2021-09-07 17:11:36 +0200

    squashme: move git clone back to init container

--- renku_notebooks/config.py
+++ renku_notebooks/config.py
@@ -46,13 +46,18 @@ DEFAULT_IMAGE = os.environ.get("NOTEBOOKS_DEFAULT_IMAGE", "renku/singleuser:late
 """The default image to use for an interactive session if the image tied to the
 current commit cannot be found."""
 
-GIT_SIDECAR_IMAGE = os.environ.get("GIT_SIDECAR_IMAGE", "renku/git-sidecar:latest")
+GIT_RPC_SERVER_IMAGE = os.environ.get(
+    "GIT_RPC_SERVER_IMAGE", "renku/git-sidecar:latest"
+)
 """The image used to clone the git repository when a user session is started"""
 
 GIT_HTTPS_PROXY_IMAGE = os.environ.get(
     "GIT_HTTPS_PROXY_IMAGE", "renku/git-https-proxy:latest"
 )
 """The HTTPS proxy sidecar container image."""
+
+GIT_CLONE_IMAGE = os.environ.get("GIT_CLONE_IMAGE", "renku/git-clone:latest")
+"""The git clone init container image."""
 
 NOTEBOOKS_SESSION_PVS_ENABLED = (
     os.environ.get("NOTEBOOKS_SESSION_PVS_ENABLED", "false") == "true"
