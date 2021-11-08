f854d3491de8bead044ae61e9c0d53c74e19927b
Tasko Olevski <olevski90@gmail.com>
2021-11-08 11:12:36 +0100

    fix: add ingress annotation to each session so iframes work (#794)

--- renku_notebooks/api/classes/server.py
+++ renku_notebooks/api/classes/server.py
@@ -727,6 +727,26 @@ class UserServer:
                     ],
                 }
             )
+        patches.append(
+            {
+                "type": "application/json-patch+json",
+                "patch": [
+                    {
+                        "op": "add",
+                        # "~1" == "/" for rfc6902 json patches
+                        "path": (
+                            "/ingress/metadata/annotations/"
+                            "nginx.ingress.kubernetes.io~1configuration-snippet"
+                        ),
+                        "value": (
+                            'more_set_headers "Content-Security-Policy: '
+                            "frame-ancestors 'self' "
+                            f'{self.server_url}";'
+                        ),
+                    }
+                ],
+            }
+        )
         if current_app.config["NOTEBOOKS_SESSION_PVS_ENABLED"]:
             storage = {
                 "size": self.server_options["disk_request"],
