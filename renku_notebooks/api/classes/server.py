aa7917ab7b59b028d9f5fa3b21f143f11e8d9e44
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2021-11-15 10:42:02 +0100

    feat(chart): session tolerations, affinities, nodeSelector

--- renku_notebooks/api/classes/server.py
+++ renku_notebooks/api/classes/server.py
@@ -306,14 +306,6 @@ class UserServer:
             + "projectName": self.gl_project.path.lower(),
             f"{current_app.config['RENKU_ANNOTATION_PREFIX']}requested-image": self.image,
         }
-        tolerations = [
-            {
-                "key": f"{current_app.config['RENKU_ANNOTATION_PREFIX']}dedicated",
-                "operator": "Equal",
-                "value": "user",
-                "effect": "NoSchedule",
-            }
-        ]
         # Add image pull secret if image is private
         if self.is_image_private:
             image_pull_secret_name = self.server_name + "-image-secret"
@@ -565,7 +557,31 @@ class UserServer:
                     {
                         "op": "add",
                         "path": "/statefulset/spec/template/spec/tolerations",
-                        "value": tolerations,
+                        "value": current_app.config.SESSION_TOLERATIONS,
+                    }
+                ],
+            }
+        )
+        patches.append(
+            {
+                "type": "application/json-patch+json",
+                "patch": [
+                    {
+                        "op": "add",
+                        "path": "/statefulset/spec/template/spec/affinity",
+                        "value": current_app.config.SESSION_AFFINITY,
+                    }
+                ],
+            }
+        )
+        patches.append(
+            {
+                "type": "application/json-patch+json",
+                "patch": [
+                    {
+                        "op": "add",
+                        "path": "/statefulset/spec/template/spec/nodeSelector",
+                        "value": current_app.config.SESSION_NODE_SELECTOR,
                     }
                 ],
             }
