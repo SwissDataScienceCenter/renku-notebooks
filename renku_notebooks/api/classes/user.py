0a151b0f00b4f6dedd3275da35944ae4e7183bd2
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2021-11-22 23:40:28 +0100

    squashme: fix formatting

--- renku_notebooks/api/classes/user.py
+++ renku_notebooks/api/classes/user.py
@@ -116,9 +116,7 @@ class RegisteredUser(User):
     @staticmethod
     def parse_jwt_from_headers(headers):
         # No need to verify the signature because this is already done by the gateway
-        return jwt.decode(
-            headers["Renku-Auth-Id-Token"], verify=False
-        )
+        return jwt.decode(headers["Renku-Auth-Id-Token"], verify=False)
 
     @staticmethod
     def git_creds_from_headers(headers):
