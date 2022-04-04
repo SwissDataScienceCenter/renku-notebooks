61d9a72367003853ad1ed8390db0feb0fbf31f28
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2022-04-04 23:00:02 +0200

    feat(app): switch git proxy to golang

---
+++ git-https-proxy/main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/elazarl/goproxy"
+)
+
+func main() {
+	config := parseEnv()
+	proxyHandler := getProxyHandler(config)
+	proxyServer := http.Server{
+		Addr:    fmt.Sprintf(":%s", config.ProxyPort),
+		Handler: proxyHandler,
+	}
+	healthHandler := getHealthHandler(config)
+	healthServer := http.Server{
+		Addr:    fmt.Sprintf(":%s", config.HealthPort),
+		Handler: healthHandler,
+	}
+	go func() {
+		// Run the health server in the "background"
+		log.Fatal(healthServer.ListenAndServe())
+	}()
+	log.Fatalln(proxyServer.ListenAndServe())
+}
+
+type gitProxyConfig struct {
+	ProxyPort          string
+	HealthPort         string
+	AnonymousSession   bool
+	EncodedCredentials string
+	RepoUrl            *url.URL
+}
+
+// Parse the environment variables used as the configuration for the proxy.
+func parseEnv() *gitProxyConfig {
+	var ok, anonymousSession bool
+	var gitlabOauthToken, proxyPort, healthPort, anonymousSessionStr, encodedCredentials string
+	var repoUrl *url.URL
+	if proxyPort, ok = os.LookupEnv("MITM_PROXY_PORT"); !ok {
+		proxyPort = "8080"
+	}
+	if healthPort, ok = os.LookupEnv("HEALTH_PORT"); !ok {
+		healthPort = "8081"
+	}
+	if anonymousSessionStr, ok = os.LookupEnv("ANONYMOUS_SESSION"); !ok {
+		anonymousSessionStr = "true"
+	}
+	anonymousSession = anonymousSessionStr == "true"
+	gitlabOauthToken = os.Getenv("GITLAB_OAUTH_TOKEN")
+	encodedCredentials = encodeCredentials(gitlabOauthToken)
+	repoUrl, err := url.Parse(os.Getenv("REPOSITORY_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &gitProxyConfig{
+		ProxyPort:          proxyPort,
+		HealthPort:         healthPort,
+		AnonymousSession:   anonymousSession,
+		EncodedCredentials: encodedCredentials,
+		RepoUrl:            repoUrl,
+	}
+}
+
+func encodeCredentials(token string) string {
+	return base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("oauth2:%s", token)))
+}
+
+// Return a server handler that contains the proxy that injects the Git aithorization header when
+// the conditions for doing so are met.
+func getProxyHandler(config *gitProxyConfig) *goproxy.ProxyHttpServer {
+	proxyHandler := goproxy.NewProxyHttpServer()
+	proxyHandler.Verbose = true
+	proxyHandler.OnRequest().DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			var validGitRequest bool
+			var hostsMatch, requestContainsWww, configContainsWww bool
+			var err error
+			// NOTE: Ensure www.hostname.com should match hostname.com and vice versa
+			wwwRegex := fmt.Sprintf("^%s", regexp.QuoteMeta("www."))
+			requestContainsWww, err = regexp.MatchString(wwwRegex, r.URL.Hostname())
+			if err != nil {
+				log.Fatalln(err)
+			}
+			configContainsWww, err = regexp.MatchString(wwwRegex, config.RepoUrl.Hostname())
+			if err != nil {
+				log.Fatalln(err)
+			}
+			if configContainsWww && !requestContainsWww {
+				hostsMatch = fmt.Sprintf("www.%s", r.URL.Hostname()) == config.RepoUrl.Hostname()
+			} else if !configContainsWww && requestContainsWww {
+				hostsMatch = r.URL.Hostname() == fmt.Sprintf("www.%s", config.RepoUrl.Hostname())
+			} else {
+				hostsMatch = r.URL.Hostname() == config.RepoUrl.Hostname()
+			}
+			validGitRequest = r.URL.Scheme == config.RepoUrl.Scheme && hostsMatch && r.URL.Port() == config.RepoUrl.Port() && strings.HasPrefix(strings.TrimLeft(r.URL.Path, "/"), strings.TrimLeft(config.RepoUrl.Path, "/"))
+			if config.AnonymousSession {
+				log.Print("Anonymous session, not adding auth headers, letting request through without adding auth headers.\n")
+				return r, nil
+			}
+			if !validGitRequest {
+				log.Printf("The request %v does not match the git repository %v, letting request through without adding auth headers\n", r.URL, config.RepoUrl)
+				return r, nil
+			}
+			log.Printf("Adding auth header to request: %v\n", r.URL)
+			r.Header.Set("Authorization", fmt.Sprintf("Basic %s", config.EncodedCredentials))
+			return r, nil
+		})
+	return proxyHandler
+}
+
+// The proxy does not expose a health endpoint. Therefore the purpose of this server
+// handler is to just fill that functionality. To ensure that the proxy is fully up
+// and running the health server will use the proxy as a proxy for the health endpoint.
+// This is necessary because sending any requests directly to the proxy results in a 500
+// with a message that the proxy only accepts proxy requests and no direct requests.
+func getHealthHandler(config *gitProxyConfig) *http.ServeMux {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		resp := make(map[string]string)
+		resp["message"] = "pong"
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+		}
+		w.Write(jsonResp)
+	})
+	handler.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		proxyUrl, err := url.Parse(fmt.Sprintf("localhost:%s", config.ProxyPort))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+		resp, err := client.Get(fmt.Sprintf("localhost:%s/ping", config.HealthPort))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode >= 200 && resp.StatusCode <= 400 {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	})
+
+	return handler
+}
