caf86dd2ca0e0600db47112395ae0e21e32f973f
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2022-04-13 10:22:17 +0200

    chore: run as non-root

--- renku_notebooks/api/amalthea_patches/git_proxy.py
+++ renku_notebooks/api/amalthea_patches/git_proxy.py
@@ -20,6 +20,12 @@ def main(server):
                     "path": "/statefulset/spec/template/spec/containers/-",
                     "value": {
                         "image": current_app.config["GIT_HTTPS_PROXY_IMAGE"],
+                        "securityContext": {
+                            "fsGroup": 100,
+                            "runAsGroup": 100,
+                            "runAsUser": 1000,
+                            "allowPrivilegeEscalation": False,
+                        },
                         "name": "git-proxy",
                         "env": [
                             {
