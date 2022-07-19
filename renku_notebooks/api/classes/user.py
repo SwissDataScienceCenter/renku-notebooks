7209f0be433318d84d03b7083485694f11b60295
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2022-07-19 10:32:02 +0200

    chore(app): fix for updating gitlab sdk to 3.6.0

--- renku_notebooks/api/classes/user.py
+++ renku_notebooks/api/classes/user.py
@@ -122,11 +122,9 @@ class RegisteredUser(User):
 
     @property
     def gitlab_user(self):
-        try:
-            return self.gitlab_client.user
-        except AttributeError:
+        if not getattr(self.gitlab_client, "user", None):
             self.gitlab_client.auth()
-            return self.gitlab_client.user
+        return self.gitlab_client.user
 
     @staticmethod
     def parse_jwt_from_headers(headers):
