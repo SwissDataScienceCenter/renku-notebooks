9300ba2744d2d8322425fa103d4b493518c291d0
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2024-05-02 23:49:39 +0200

    fix: project path case sensitivity

--- renku_notebooks/api/classes/server.py
+++ renku_notebooks/api/classes/server.py
@@ -118,6 +118,8 @@ class UserServer:
 
     @property
     def gl_project_path(self) -> Optional[str]:
+        # NOTE: This is case sensitive and will reflect exact lower/uppercase combination of letters
+        # that is used in Gitlab for the path of the project.
         gl_project = self.gl_project
         return gl_project.path if gl_project else None
 
@@ -133,7 +135,7 @@ class UserServer:
                 asdict(
                     Repository(
                         namespace=self.namespace,
-                        project=self.project,
+                        project=self.gl_project_path,
                         branch=self.branch,
                         commit_sha=self.commit_sha,
                         url=self.gl_project_url,
