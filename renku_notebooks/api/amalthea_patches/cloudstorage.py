97bebbba6fba5983eef6014b23eb97156b08ef3f
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-05-28 14:29:06 +0200

    fix the fix

--- renku_notebooks/api/amalthea_patches/cloudstorage.py
+++ renku_notebooks/api/amalthea_patches/cloudstorage.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
 def main(server: "UserServer") -> list[dict[str, Any]]:
     cloud_storage_patches: list[dict[str, Any]] = []
     cloud_storage_request: "ICloudStorageRequest"
-    if not server.cloudstorage or not server.repositories:
+    if not server.cloudstorage:
         return []
     for i, cloud_storage_request in enumerate(server.cloudstorage):
         cloud_storage_patches.extend(
@@ -16,19 +16,20 @@ def main(server: "UserServer") -> list[dict[str, Any]]:
                 f"{server.server_name}-ds-{i}", server.k8s_client.preferred_namespace
             )
         )
-        cloud_storage_patches.append(
-            {
-                "type": "application/json-patch+json",
-                "patch": [
-                    {
-                        "op": "add",
-                        "path": "/statefulset/spec/template/spec/initContainers/2/env/-",
-                        "value": {
-                            "name": f"GIT_CLONE_STORAGE_MOUNTS_{i}",
-                            "value": cloud_storage_request.mount_folder,
+        if server.repositories:
+            cloud_storage_patches.append(
+                {
+                    "type": "application/json-patch+json",
+                    "patch": [
+                        {
+                            "op": "add",
+                            "path": "/statefulset/spec/template/spec/initContainers/2/env/-",
+                            "value": {
+                                "name": f"GIT_CLONE_STORAGE_MOUNTS_{i}",
+                                "value": cloud_storage_request.mount_folder,
+                            },
                         },
-                    },
-                ],
-            },
-        )
+                    ],
+                },
+            )
     return cloud_storage_patches
