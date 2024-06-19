792bb2d0fa55c081f0ce70cbd366a3546ee1e238
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2024-06-19 10:19:26 +0200

    fix formatting

--- renku_notebooks/api/amalthea_patches/cloudstorage.py
+++ renku_notebooks/api/amalthea_patches/cloudstorage.py
@@ -7,7 +7,7 @@ if TYPE_CHECKING:
 
 def main(server: "UserServer") -> list[dict[str, Any]]:
     cloud_storage_patches: list[dict[str, Any]] = []
-    cloud_storage_request: "ICloudStorageRequest"
+    cloud_storage_request: ICloudStorageRequest
     if not server.cloudstorage:
         return []
     for i, cloud_storage_request in enumerate(server.cloudstorage):
